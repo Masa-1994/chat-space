575da2d97d57efe52c304b152f2b7e294ec56daa
Masa-1994 <kyani_m1994@icloud.com>
2019-10-28 18:55:59 +0900

    creating_message_js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,26 @@
 $(function(){
+  function buildHTML(message) {
+    var content = message.content ? `${ message.content }` : "";
+    var img = message.image ? `<img src= ${ message.image }>` : "";
+    var html = `<div class="message" data-id="${message.id}">
+                  <div class="message__detail">
+                    <p class="message__detail__current-user-name">
+                      ${message.user_name}
+                    </p>
+                    <p class="message__detail__date">
+                      ${message.created_at}
+                    </p>
+                  </div>
+                  <p class="message_body">
+                    <div>
+                    ${content}
+                    </div>
+                    ${img}
+                  </p>
+                </div>`
+  return html;
+  }
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -10,6 +32,14 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('#message_content').val(''); //input内のメッセージを消しています。
+    })
+    .fail(function(data){
+      alert('エラーが発生したためメッセージは送信できませんでした。');
     })
   })
 });
